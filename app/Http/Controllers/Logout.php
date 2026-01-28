1e1a69df03505d00e1123ae4abe74122335c9e35
Lahmar22 <lahmarzakariae22@gmail.com>
2026-01-28 12:29:20 +0100

    feat : add the controller logout user

---
+++ app/Http/Controllers/Logout.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class Logout extends Controller
+{
+    public function logout()
+    {
+        Session::flush();
+        Session::regenerate();
+        return redirect('/login');
+    }
+}
