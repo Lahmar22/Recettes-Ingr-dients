3ea6b62ff6adff72953c9299b5ca1681f6fa3443
Lahmar22 <lahmarzakariae22@gmail.com>
2026-01-28 12:38:53 +0100

    feat : update web router

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,37 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\loginContoller;
+use App\Http\Controllers\Logout;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InscriptionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('login', function () {
+Route::get('/login', function () {
     return view('login');
-});
+})->name('login');
 
 Route::get('inscription', function () {
     return view('inscription');
 });
+
+Route::get('home', function () {
+    return view('home');
+});
+
+Route::get('/home', function () {
+    // return "Welcome " . session('user')->nom_user;
+    return view('home');
+})->name('home');
+
+Route::get('recete', function () {
+    return view('recete');
+});
+
+Route::post('/inscriptionUser', [InscriptionController::class, 'store']);
+Route::post('/logincontroller', [loginContoller::class, 'login']);
+
+Route::post('/logout', [Logout::class, 'logout']);
