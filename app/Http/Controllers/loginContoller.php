457bb6ba302e042844e6fcbc952765827d29945d
Lahmar22 <lahmarzakariae22@gmail.com>
2026-01-28 12:29:04 +0100

    feat : add the controller login user

---
+++ app/Http/Controllers/loginContoller.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Utilisateur;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+class loginContoller extends Controller
+{
+    public function login(Request $request){
+
+        
+        $user = Utilisateur::where('email_user', $request->email)->first();
+
+        if (!$user) {
+        return back()->with('error', 'Email not found');
+    }
+
+
+        if (!Hash::check($request->password, $user->password_user)) {
+        return back()->with('error', 'Incorrect password');
+    }
+
+    Session::put('user', $user);
+
+    return redirect()->route('home');
+
+    }
+}
